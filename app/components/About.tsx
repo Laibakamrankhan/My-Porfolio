e635d9c0c8395fc3793c4dda643b847ea6e4200f
Laibakamrankhan <160596709+Laibakamrankhan@users.noreply.github.com>
2024-11-10 23:23:22 +0500

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div id='about' className='container pt-32'>
         <h2 className='text-4xl md:text-5xl'>About Me</h2>
         <p className='text-gray-500 pt-4'>
-        Currently pursuing a Bachelor's degree in Nutrition and Dietetics with a keen interest in leveraging technology to enhance healthcare outcomes. Concurrently, I am enrolled in a course on Cloud Applied Generative AI, aiming to merge my passion for nutrition with cutting-edge AI technologies.
+        Currently pursuing a Bachelors degree in Nutrition and Dietetics with a keen interest in leveraging technology to enhance healthcare outcomes. Concurrently, I am enrolled in a course on Cloud Applied Generative AI, aiming to merge my passion for nutrition with cutting-edge AI technologies.
         Eager to explore innovative solutions at the intersection of nutrition, AI, and cloud computing</p>
       
     </div>
